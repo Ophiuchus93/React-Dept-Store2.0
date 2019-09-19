1ee6b700ca00bb4523f112643334f4c7187b5e3e
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-09-19 09:17:50 -0600

    ran into a json issue, couldn't get further

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,38 @@
 import React from "react";
-import { Header, } from "semantic-ui-react"
+import { Header, Card,  } from "semantic-ui-react"
 
-const Home = () => (
-  <div>
-    Home
-  </div>
-);
+class Home extends React.Component {
+  state = { departments: [], }; 
+
+  componentDidMount () {
+    
+  }
+
+  renderDepartments = () => {
+    const { departments } = this.state;
+    if (departments.length <= 0)
+      return <h2>No Departments</h2>
+        return departments.map( department => (
+          <Card>
+            <Card.Content>
+              <Card.Header>{ department.name }</Card.Header>
+            </Card.Content>
+          </Card>
+        ))
+  }
+
+
+  render () {
+    return (
+      <div>
+        <Header as="h1">Departments</Header>
+        <br />
+        <Card.Group>
+          { this.renderDepartments() }
+        </Card.Group>
+      </div>
+    )
+  }
+}
 
 export default Home;
